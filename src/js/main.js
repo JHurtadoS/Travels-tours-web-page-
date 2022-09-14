ac0b9f9508c80d5f519ccad453e5200636d9b25e
JHurtadoS <juaneshs2014@gmail.com>
2022-09-14 13:18:09 -0500

    Intento de detener scroll al acceder al pop-up

--- src/js/main.js
+++ src/js/main.js
@@ -36,8 +36,11 @@ function pop_up_show() {
     button[0].addEventListener("click", (e) => {
       console.log(`pop-up-${id}`);
       const pop_up = document.getElementById(`pop-up-${id}`);
-      console.log(pop_up);
+      //console.log(pop_up);
       pop_up.classList.add("open");
+      //stopscroll("disable");
+      disableScroll();
+      document.body.classList.add("stop_scrolling");
     });
   });
 
@@ -46,6 +49,66 @@ function pop_up_show() {
     // console.log(icons);
     icons[0].addEventListener("click", (e) => {
       element.classList.remove("open");
+      document.body.classList.remove("stop_scrolling");
+      enableScroll();
+      //stopscroll("enable");
     });
   });
+}
+/*
+function stopscroll(action) {
+  var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+  function preventDefault(e) {
+    e.preventDefault();
+  }
+
+  function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  }
+
+  // modern Chrome requires { passive: false } when adding event
+  var supportsPassive = false;
+  try {
+    window.addEventListener(
+      "test",
+      null,
+      Object.defineProperty({}, "passive", {
+        get: function () {
+          supportsPassive = true;
+        },
+      })
+    );
+  } catch (e) {}
+
+  var wheelOpt = supportsPassive ? { passive: false } : false;
+  var wheelEvent =
+    "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+
+  if (action == "disable") {
+    window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+    window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+    console.log("here1");
+  } else if (action == "enable") {
+    console.log("here2");
+    window.removeEventListener("DOMMouseScroll", preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener("touchmove", preventDefault, wheelOpt);
+    window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+  }
+}
+*/
+function disableScroll() {
+  window.onscroll = () => {
+    window.scroll(0, 0);
+  };
+}
+
+function enableScroll() {
+  window.onscroll = "";
 }
